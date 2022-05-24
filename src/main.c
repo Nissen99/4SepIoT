a2951b816deb2d967d6b3dbd152c9a657eb69575
Nissen99 <mikkelnj411@gmail.com>
2022-05-24 15:34:49 +0200

    Cleanup

--- src/main.c
+++ src/main.c
@@ -45,30 +45,12 @@ MessageBufferHandle_t downLinkMessageBufferHandle;
 
 int main() {
 	
-	// Here I make room for two downlink messages in the message buffer TODO
-
-	//downlinkMessageBuffer = xMessageBufferCreate(sizeof(lora_driver_payload_t) * 2);
-	
-	// The parameter is the USART port the RN2483 module is connected to -
-	// in this case USART1 - here no message buffer for down-link messages are defined
-	
-	//lora_driver_initialise(ser_USART1, downLinkMessageBufferHandle);
-	//xTaskCreate(loraWan_TaskReset,"task reset", configMINIMAL_STACK_SIZE,NULL,8,NULL);
-	
-	
 	
 	// Set output ports for leds used in the example
-	DDRA |= _BV(DDA0) | _BV(DDA7);
+	//DDRA |= _BV(DDA0) | _BV(DDA7);
+	
 	// Initialise the trace-driver to be used together with the R2R-Network
 	trace_init();
-
-	// vvvvvvvvvvvvvvvvv BELOW IS LoRaWAN initialisation vvvvvvvvvvvvvvvvvvvvvvvvvvvvvv
-	// Initialise the HAL layer and use 5 for LED driver priority
-	//hal_create(5);
-	
-	// Create LoRaWAN task and start it up with priority 3
-	// lora_handler_create(3);
-	
 
 	// Make it possible to use stdio on COM port 0 (USB) on Arduino board - Setting 57600,8,N,1
 	// for at printe ud skal dette her med ( header filen "stdio_driver.h" skal includes
@@ -76,22 +58,21 @@ int main() {
 	
 	downLinkMessageBufferHandle = xMessageBufferCreate(sizeof(lora_driver_payload_t) * 2);
 	
-	//downLinkMessageBufferHandle = xMessageBufferCreate(sizeof(lora_driver_payload_t)*2);
-
 	
+	// Innitalisere
+	semaphore = xSemaphoreCreateMutex();
 	
-	// Initialise the LoRaWAN driver without down-link buffer
-	//lora_driver_initialise(1, NULL);
-	//setup for loRaWAN
 	lora_driver_initialise(ser_USART1,downLinkMessageBufferHandle);
+
+	if(HIH8120_OK !=  hih8120_initialise()) {
+		printf("Failed to initialize temperature sensor\n");
+		return 1;
+	}
 	
-	//lora_handler_task(3);
-
-
-
-	//vi starter med at sætte alle variablerne til 0
-	//terrariumdata.humidity = 0;
-	//terrariumdata.temperature = 0;
+	mh_z19_initialise(ser_USART3);
+	rc_servo_initialise();
+	init_downlink_handler(downLinkMessageBufferHandle);
+	
 	
 	
 	//opretter de Task vi skal lave ( vha. FreeRTOS)
@@ -99,23 +80,8 @@ int main() {
 	xTaskCreate(lora_handler_task, "Led", configMINIMAL_STACK_SIZE, NULL,TEMP_TASK_PRIORITY+1, &loRaWanHandle);
 	xTaskCreate(co2SensorTask, "co2Mesurement", configMINIMAL_STACK_SIZE, NULL,TEMP_TASK_PRIORITY, &co2SensorHandle);
 	xTaskCreate(lora_downlink_handler_task, "loraDownLink", configMINIMAL_STACK_SIZE, NULL,TEMP_TASK_PRIORITY, &loradownlink);
-	
-	
-	//xTaskCreate()
 
 
-	// Innitalisere semaphoren
-	semaphore = xSemaphoreCreateMutex();
-	
-	// initialalisere temp/fugt måleren
-	if(HIH8120_OK !=  hih8120_initialise()) {
-		printf("Failed to initialize temperature sensor\n");
-		return 1;
-	}
-	mh_z19_initialise(ser_USART3);
-	rc_servo_initialise();
-	init_downlink_handler(downLinkMessageBufferHandle);
-	
 	// der må ikke køres kode, når scheduleren er eksekveret
 	vTaskStartScheduler();
 
